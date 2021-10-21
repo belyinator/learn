d5b92978d5c1c6c5b67b5b578543302d1e628105
Nikita <92950420+belyinator@users.noreply.github.com>
2021-10-21 21:48:09 +0200

    Add files via upload

---
+++ Площадевычислитель.Квадрат.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+    int a,b;
+    cout << "STORONA" << endl; 
+    cin>> a;
+    b=a*a;
+    
+    cout << "SQUARE" << endl <<  b << std::endl;
+
+    
+    
+    
+    return 0;
+}
